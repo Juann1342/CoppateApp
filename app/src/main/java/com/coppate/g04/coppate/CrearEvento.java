217691500b6c122957c0c3a063b7d8249a382d66
Julian Arsuaga <arsuaga00@gmail.com>
2016-10-11 22:41:58 -0300

    Se agrega un activity temporal para seleccionar un contacto solo para no perder el código

--- app/src/main/java/com/coppate/g04/coppate/CrearEvento.java
+++ app/src/main/java/com/coppate/g04/coppate/CrearEvento.java
@@ -1,7 +1,13 @@
 package com.coppate.g04.coppate;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,11 +15,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 
-public class CrearEvento extends AppCompatActivity {
+public class CrearEvento extends Activity {
 
     //definimos los componenetes que va a tener la clase y que despues pueden llamarse para operar
     EditText nombre_evento;
@@ -35,10 +42,17 @@ public class CrearEvento extends AppCompatActivity {
     String sexo = "";
     String tipo = "";
 
+    private static final int RESULT_PICK_CONTACT = 85500;
+    private TextView textView1;
+    private TextView textView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_evento);
+
+        textView1 = (TextView) findViewById(R.id.ce_lista_coppados);
+        textView2 = (TextView) findViewById(R.id.ce_lista_coppados2);
 
         //defino las posibles opciones que pueden tener los desplegables (esto despues lo tengo que reemplazar con lo que traigo
         // de la base de datos)
@@ -109,6 +123,16 @@ public class CrearEvento extends AppCompatActivity {
             }
         });
 
+        /*
+        btn_invitar_contactos.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+              public void onClick(View v) {
+                Intent intent_invitCopp = new Intent(CrearEvento.this,InvitarCoppados.class);
+                startActivity(intent_invitCopp);
+            }
+        });*/
+
         //al hacer click en el boton, nos mostrara el texto en pantalla que se ha creado un evento
         // con el nombre que hemos ingresado
         btn_crear.setOnClickListener(new View.OnClickListener() {
@@ -135,6 +159,53 @@ public class CrearEvento extends AppCompatActivity {
 
         toast2.setGravity(Gravity.CENTER|Gravity.LEFT,0,0);
 
+    }
+
+    public void pickContact(View v)
+    {
+        // creamos un StartActivityForResult para mostrar los contactos
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // comprobamos si el resultado esta ok
+        if (resultCode == RESULT_OK) {
+            // Comprobar si el codigo de peticion, podriamos estar utilizando múltiples startActivityForResult (distinto a StartActivity)
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Log.e("MainActivity", "Failed to pick contact");
+        }
+    }
+
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+        try {
+            String phoneNo = null ;
+            String name = null;
+            // el metodo GetData() va a tener el contenido URI del contacto seleccionado.
+            Uri uri = data.getData();
+            // consultamos el contenido de la URI
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+
+            // tomamos los indices de las columnas del numero y el nombre de los telefonos
+            int  phoneIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+            int  nameIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            phoneNo = cursor.getString(phoneIndex);
+            name = cursor.getString(nameIndex);
+            // Establecemos el valor de los textviews
+            textView1.setText(name);
+            textView2.setText(phoneNo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
